74b58ed040066bacf3d1b385a272e83fc6e2cfee
Oren-Schmerling <orenschmerling@gmail.com>
2025-09-28 17:03:31 -0400

    started working on upload

---
+++ backend/src/main/java/waxwing/campusbike/auth/registrationUtil.java
@@ -0,0 +1,13 @@
+package waxwing.campusbike.auth;
+import waxwing.campusbike.auth.passwordUtil;
+import waxwing.campusbike.types.User;
+import org.apache.commons.validator.routines.EmailValidator;
+
+public class registrationUtil {
+    public static String verifyEmail (String email) {
+
+    }
+    public static boolean uploadUser( User user) {
+
+    }
+}
