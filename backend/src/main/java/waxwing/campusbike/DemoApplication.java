51f0968764210212af2e6d3042d7d9637aead580
Oren-Schmerling <orenschmerling@gmail.com>
2025-09-29 10:50:56 -0400

    added post routing

--- backend/src/main/java/waxwing/campusbike/DemoApplication.java
+++ backend/src/main/java/waxwing/campusbike/DemoApplication.java
@@ -1,9 +1,14 @@
 package waxwing.campusbike;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import waxwing.campusbike.auth.registrationUtil;
 
 @SpringBootApplication
 @RestController
@@ -14,5 +19,15 @@ public class DemoApplication {
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
+    }
+
+    @PostMapping("/users")
+    public int register(
+      @RequestParam(value = "username") String username,
+      @RequestParam(value = "pwHash") String pwHash,
+      @RequestParam(value = "email") String email,
+      @RequestParam(value = "phone", defaultValue = "") String phone
+    ) throws URISyntaxException, IOException{
+      return registrationUtil.registrationHandler(username, pwHash, email, phone);
     }
 }
