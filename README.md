f89dbbbec3c63fa83e3051e4f56da7a7c150aa29
Oren-Schmerling <orenschmerling@gmail.com>
2025-09-29 01:17:33 -0400

    registration backend with new database API

--- README.md
+++ README.md
@@ -6,10 +6,12 @@ Campus bike rental project made by group Waxwing
 
 SpringBoot is set to run in this repository on Java 21. Please install Java 21 or a newer version.
 
-The code is set up to run with `docker-compose`. Please install the latest stable version of Docker. Once installed, you should be able to run `docker-compose up --build` in the root directory of the repository and it will run the backend as well as the PostgreSQL db.
+The code is set up to run with `docker-compose`. Please install the latest stable version of Docker. Once installed, you should be able to run `docker-compose up` in the root directory of the repository and it will run the backend as well as the PostgreSQL db. Using the `--build` flag on `docker-compose up` will now fail since some tests depend on the other containers being running and healthy.
 
 After starting up the volumes on docker, you should be able to see "Hello World!" printed on your screen at http://localhost:8080/hello
 
 You can look at the PostgreSQL database by logging into it. It's on port `5432` and you can connect to it with the following command: `psql -h localhost -p 5432 -U postgres -d bikedb`. You'll be prompted to enter the password which is "postgres" for now.
 
-When you're done running the server, be sure to stop the process and then run `docker-compose down` to tear down the docker images.
+@todo: document postgrest
+
+When you're done running the server, be sure to stop the process and then run `docker-compose down -v` to tear down the docker images.
---
+++ backend/src/test/java/waxwing/campusbike/registrationUtilTests.java
@@ -0,0 +1,60 @@
+package waxwing.campusbike;
+import waxwing.campusbike.auth.registrationUtil;
+import org.junit.jupiter.api.Test;
+import waxwing.campusbike.auth.EmailValidity;
+import waxwing.campusbike.auth.passwordUtil; 
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+
+public class registrationUtilTests {
+
+  @Test
+  void testEmailValidityInvalid(){
+    assertEquals(registrationUtil.verifyEmail("notAnEmail"), EmailValidity.INVALID);
+  }
+
+  @Test
+  void testEmailValidityNonUMass(){
+    assertEquals(registrationUtil.verifyEmail("normalemail@gmail.com"), EmailValidity.NOT_UMASS);
+  }
+
+  @Test
+  void testEmailValidityValid(){
+    assertEquals(registrationUtil.verifyEmail("normalemail@umass.edu"), EmailValidity.VALID);
+  }
+
+  @Test
+  void testRegistrationHandlerSuccess() throws URISyntaxException, IOException{
+    String username = UUID.randomUUID().toString();
+    String hashedPassword = passwordUtil.hashPassword("password");
+    String email = username + "@umass.edu";
+    int result =registrationUtil.registrationHandler(
+      username,
+      hashedPassword,
+      email,
+      ""
+      );
+    boolean responseOk = (result >= 200 && result < 300);
+    assertTrue(responseOk);
+  }
+
+  @Test
+  void testRegistrationHandlerFailure() throws URISyntaxException, IOException{
+    String username = UUID.randomUUID().toString();
+    String hashedPassword = passwordUtil.hashPassword("password");
+    String email = username + "@gmail.com";
+    int result =registrationUtil.registrationHandler(
+      username,
+      hashedPassword,
+      email,
+      ""
+      );
+    boolean responseOk = (result >= 200 && result < 300);
+    assertFalse(responseOk);
+  }
+  
+}
