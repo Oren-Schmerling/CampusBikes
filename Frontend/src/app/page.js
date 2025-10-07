461c16312e938415735a5e498ebd8fe3e0c7cf92
Oren-Schmerling <orenschmerling@gmail.com>
2025-10-06 21:39:17 -0400

    added visuals to landing page

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -1,103 +1,125 @@
-import Image from "next/image";
+import NavBar from "@/components/nav/navBar";
+
+function BigText() {
+  return (
+    <div className="justify-start z-100">
+      <div className="w-full">
+        <span className="text-nearBlack text-6xl font-semibold font-['Poppins'] leading-[72px] tracking-wide">
+          Use{" "}
+        </span>
+        <span className="text-waxwingGreen text-6xl font-semibold font-['Poppins'] leading-[72px] tracking-wide">
+          CampusBikes
+        </span>
+      </div>
+      <div className="w-full">
+        <span className="text-nearBlack text-6xl font-semibold font-['Poppins'] leading-[72px] tracking-wide">
+          {" "}
+          get around{" "}
+        </span>
+        <span className="text-waxwingGreen text-6xl font-semibold font-['Poppins'] leading-[72px] tracking-wide">
+          Greener{" "}
+        </span>
+      </div>
+      <div className="w-full">
+        <span className="text-nearBlack text-6xl font-semibold font-['Poppins'] leading-[72px] tracking-wide">
+          rent wheels{" "}
+        </span>
+        <span className="text-waxwingGreen text-6xl font-semibold font-['Poppins'] leading-[72px] tracking-wide">
+          Safer
+        </span>
+      </div>
+    </div>
+  );
+}
+
+function LittleText() {
+  return (
+    <div className="w-[512px] justify-start text-nearBlack text-2xl font-normal font-['Poppins'] leading-9 z-100">
+      Local bike and scooter rentals for students, by students.{" "}
+    </div>
+  );
+}
+
+function JoinButton() {
+  return (
+    <div className="w-85 px-14 py-6 bg-waxwingGreen rounded-[40px] z-100">
+      <div className="justify-self-center text-lighterGray text-2xl font-medium font-['Poppins']">
+        Join the community
+      </div>
+    </div>
+  );
+}
+
+function BikeImage() {
+  return (
+    <img className="w-[700px] h-[459px] rounded-[81px]" src="bikeImg.jpg" />
+  );
+}
+
+function LowerSection() {
+  return (
+    <div>
+      <div className="flex row w-full justify-center absolute bottom-25 gap-25">
+        <div className="w-110 h-28 relative rounded-2xl outline outline-1 outline-offset-[-1px] outline-nearBlack backdrop-blur-lg overflow-hidden">
+          <div className="left-[30px] top-[16px] absolute inline-flex justify-start items-start gap-6">
+            <div data-style="Check" className="w-20 h-20 relative">
+              <div className="w-20 h-20 left-0 top-0 absolute bg-waxwingGreen rounded-[100px]"></div>
+              <div className="w-25 h-25 left-[28px] top-[28px] absolute">
+                <img src="check.svg" alt="check mark icon" />
+              </div>
+            </div>
+            <div className="w-64 justify-start text-nearBlack text-base font-normal font-['Poppins']">
+              We’ve created a safe space to rent to trusted peers. All users are
+              UMass verified.{" "}
+            </div>
+          </div>
+        </div>
+        <div className="w-110 h-28 relative rounded-2xl outline outline-1 outline-offset-[-1px] outline-nearBlack backdrop-blur-lg overflow-hidden">
+          <div className="left-[30px] top-[16px] absolute inline-flex justify-start items-start gap-6">
+            <div
+              data-style="Avatar"
+              className="w-20 h-20 relative bg-waxwingGreen rounded-[100px] overflow-hidden"
+            >
+              <img src="avatarCircle.svg" alt="person icon" />
+            </div>
+            <div className="w-64 justify-start text-nearBlack text-base font-normal font-['Poppins']">
+              CampusBikes is built with care by a team of UMass Computer Science
+              Students.
+            </div>
+          </div>
+        </div>
+        <div className="w-110 h-28 relative rounded-2xl outline outline-1 outline-offset-[-1px] outline-nearBlack backdrop-blur-lg overflow-hidden">
+          <div className="left-[30px] top-[16px] absolute inline-flex justify-start items-start gap-6">
+            <div className="w-20 h-20 relative overflow-hidden">
+              <img src="chat2.svg" alt="chat bubble icon" />
+            </div>
+            <div className="w-64 justify-start text-nearBlack text-base font-normal font-['Poppins']">
+              You can chat directly in our website to keep things safe and avoid
+              fraud.
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <div className="relative">
+        <NavBar />
+        <div className="flex flex-col w-1/2 bg-lighterGray rounded-br-[25%] pl-[100px] pt-[50px] pb-[100px] gap-16 relative z=50">
+          <BigText />
+          <LittleText />
+          <JoinButton />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="absolute bottom-15 right-90 -z-10">
+          <BikeImage />
+        </div>
+      </div>
+      <LowerSection />
     </div>
   );
 }
