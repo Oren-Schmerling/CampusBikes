33663f1dca0dc1a0b93b034d242c9f4023af3579
Oren-Schmerling <orenschmerling@gmail.com>
2025-10-07 13:17:14 -0400

    added cursor behavior and routing

--- Frontend/src/components/nav/navBar.js
+++ Frontend/src/components/nav/navBar.js
@@ -1,6 +1,7 @@
 import React from "react";
+import Link from "next/link";
 
-const NavItem = (icon, text) => {
+const NavItem = (icon, text, url = "/") => {
   return (
     <div
       data-badge="None"
@@ -10,13 +11,15 @@ const NavItem = (icon, text) => {
       data-state="Enabled"
       className="flex-1 self-stretch py-1.5 inline-flex flex-col justify-start items-center gap-1"
     >
-      <div className="bg-waxwingGreen rounded-2xl flex flex-col justify-center items-center overflow-hidden">
-        <div className="w-14 h-8 relative inline-flex justify-center items-center">
-          <div className="w-6 h-6 relative overflow-hidden">
-            <img src={icon} alt="Chat bubble icon"></img>
+      <Link href={url}>
+        <div className="bg-waxwingGreen hover:bg-waxwingLightGreen hover:cursor-pointer active:bg-waxwingDarkGreen rounded-2xl flex flex-col justify-center items-center overflow-hidden">
+          <div className="w-14 h-8 relative inline-flex justify-center items-center">
+            <div className="w-6 h-6 relative overflow-hidden">
+              <img src={icon} alt="Chat bubble icon"></img>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
       <div className="self-stretch text-center justify-start text-Schemes-Secondary text-xs font-medium font-['Roboto'] leading-none tracking-wide">
         {text}
       </div>
@@ -27,9 +30,9 @@ const NavItem = (icon, text) => {
 const NavBar = () => {
   return (
     <div className="w-full bg-lightGray inline-flex justify-center items-center">
-      {NavItem("accountCircle.svg", "Log in")}
-      {NavItem("plusCircle.svg", "Sign up")}
-      {NavItem("chat.svg", "Contact us")}
+      {NavItem("accountCircle.svg", "Log in", "/login")}
+      {NavItem("plusCircle.svg", "Sign up", "/signup")}
+      {NavItem("chat.svg", "Contact us", "/contact")}
     </div>
   );
 };
