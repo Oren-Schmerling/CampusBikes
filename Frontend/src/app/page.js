08263ca1f12148d1250e95f7191561ffe3b1e8d1
Tyler Rafferty <tjraff5@gmail.com>
2025-10-15 17:26:10 -0400

    refactor some of the landing page and changed dockerfile

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function BigText() {
   return (
-    <div className="justify-start z-100">
+    <div className="justify-start">
       <div className="w-full">
         <span className="text-nearBlack text-6xl font-semibold font-['Poppins'] leading-[72px] tracking-wide">
           Use{" "}
@@ -35,22 +35,63 @@ function BigText() {
 
 function LittleText() {
   return (
-    <div className="w-[512px] justify-start text-nearBlack text-2xl font-normal font-['Poppins'] leading-9 z-100">
+    <div className="w-[512px] justify-start text-nearBlack text-2xl font-normal font-['Poppins'] leading-9">
       Local bike and scooter rentals for students, by students.{" "}
     </div>
   );
 }
 
-// TODO: Link this button to the sign-up page
 function JoinButton() {
   return (
     <Link href="/signup">
-      <div className="w-85 px-14 py-6 bg-waxwingGreen rounded-[40px] z-100 hover:bg-waxwingLightGreen hover:cursor-pointer active:bg-waxwingDarkGreen active:cursor-pointer">
+      <div className="w-85 px-14 py-6 bg-waxwingGreen rounded-[40px] hover:bg-waxwingLightGreen hover:cursor-pointer active:bg-waxwingDarkGreen active:cursor-pointer">
         <div className="justify-self-center text-lighterGray text-2xl font-medium font-['Poppins']">
           Join the community
         </div>
       </div>
     </Link>
+  );
+}
+
+function FeatureCard({ icon, children }) {
+  return (
+    <div className="w-110 min-h-28 rounded-2xl outline outline-1 outline-offset-[-1px] outline-nearBlack backdrop-blur-lg">
+      <div className="px-[30px] py-4 flex justify-start items-start gap-6">
+        <div className="w-20 h-20 flex-shrink-0">
+          {icon}
+        </div>
+        <div className="flex-1 text-nearBlack text-base font-normal font-['Poppins']">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CheckIcon() {
+  return (
+    <div className="w-20 h-20 relative">
+      <div className="w-20 h-20 absolute bg-waxwingGreen rounded-full"></div>
+      <div className="w-25 h-25 absolute left-[28px] top-[28px]">
+        <img src="check.svg" alt="check mark icon" />
+      </div>
+    </div>
+  );
+}
+
+function AvatarIcon() {
+  return (
+    <div className="w-20 h-20 bg-waxwingGreen rounded-full overflow-hidden flex items-center justify-center">
+      <img src="avatarCircle.svg" alt="person icon" />
+    </div>
+  );
+}
+
+function ChatIcon() {
+  return (
+    <div className="w-20 h-20 overflow-hidden flex items-center justify-center">
+      <img src="chat2.svg" alt="chat bubble icon" />
+    </div>
   );
 }
 
@@ -60,70 +101,51 @@ function BikeImage() {
   );
 }
 
-function LowerSection() {
+function HeroSection() {
   return (
-    <div>
-      <div className="flex row w-full justify-center absolute bottom-25 gap-25">
-        <div className="w-110 h-28 relative rounded-2xl outline outline-1 outline-offset-[-1px] outline-nearBlack backdrop-blur-lg overflow-hidden">
-          <div className="left-[30px] top-[16px] absolute inline-flex justify-start items-start gap-6">
-            <div data-style="Check" className="w-20 h-20 relative">
-              <div className="w-20 h-20 left-0 top-0 absolute bg-waxwingGreen rounded-[100px]"></div>
-              <div className="w-25 h-25 left-[28px] top-[28px] absolute">
-                <img src="check.svg" alt="check mark icon" />
-              </div>
-            </div>
-            <div className="w-64 justify-start text-nearBlack text-base font-normal font-['Poppins']">
-              We’ve created a safe space to rent to trusted peers. All users are
-              UMass verified.{" "}
-            </div>
-          </div>
-        </div>
-        <div className="w-110 h-28 relative rounded-2xl outline outline-1 outline-offset-[-1px] outline-nearBlack backdrop-blur-lg overflow-hidden">
-          <div className="left-[30px] top-[16px] absolute inline-flex justify-start items-start gap-6">
-            <div
-              data-style="Avatar"
-              className="w-20 h-20 relative bg-waxwingGreen rounded-[100px] overflow-hidden"
-            >
-              <img src="avatarCircle.svg" alt="person icon" />
-            </div>
-            <div className="w-64 justify-start text-nearBlack text-base font-normal font-['Poppins']">
-              CampusBikes is built with care by a team of UMass Computer Science
-              Students.
-            </div>
-          </div>
-        </div>
-        <div className="w-110 h-28 relative rounded-2xl outline outline-1 outline-offset-[-1px] outline-nearBlack backdrop-blur-lg overflow-hidden">
-          <div className="left-[30px] top-[16px] absolute inline-flex justify-start items-start gap-6">
-            <div className="w-20 h-20 relative overflow-hidden">
-              <img src="chat2.svg" alt="chat bubble icon" />
-            </div>
-            <div className="w-64 justify-start text-nearBlack text-base font-normal font-['Poppins']">
-              You can chat directly in our website to keep things safe and avoid
-              fraud.
-              <br />
-            </div>
-          </div>
-        </div>
+    <section className="relative min-h-screen">
+      <NavBar />
+      <div className="flex flex-col w-1/2 bg-lighterGray rounded-br-[25%] pl-[100px] pt-[50px] pb-[100px] gap-16 relative z-50">
+        <BigText />
+        <LittleText />
+        <JoinButton />
       </div>
-    </div>
+      <div className="absolute bottom-15 right-90 z-10">
+        <BikeImage />
+      </div>
+    </section>
   );
 }
 
+function FeaturesSection() {
+  return (
+    <section className="w-full py-25">
+      <div className="flex flex-row w-full justify-center gap-25">
+        <FeatureCard icon={<CheckIcon />}>
+          We've created a safe space to rent to trusted peers. All users are
+          UMass verified.
+        </FeatureCard>
+
+        <FeatureCard icon={<AvatarIcon />}>
+          CampusBikes is built with care by a team of UMass Computer Science
+          Students.
+        </FeatureCard>
+
+        <FeatureCard icon={<ChatIcon />}>
+          You can chat directly in our website to keep things safe and avoid
+          fraud.
+        </FeatureCard>
+      </div>
+    </section>
+  );
+}
+
+// Main Page Component
 export default function Home() {
   return (
-    <div>
-      <div className="relative">
-        <NavBar />
-        <div className="flex flex-col w-1/2 bg-lighterGray rounded-br-[25%] pl-[100px] pt-[50px] pb-[100px] gap-16 relative z=50">
-          <BigText />
-          <LittleText />
-          <JoinButton />
-        </div>
-        <div className="absolute bottom-15 right-90 -z-10">
-          <BikeImage />
-        </div>
-      </div>
-      <LowerSection />
+    <div className="relative min-h-screen">
+      <HeroSection />
+      <FeaturesSection />
     </div>
   );
 }
