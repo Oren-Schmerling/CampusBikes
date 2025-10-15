8c01b4e0cdec17edffb554c30ebef525a6c058a5
Oren-Schmerling <orenschmerling@gmail.com>
2025-10-15 18:08:46 -0400

    start building listing frontend

---
+++ Frontend/src/app/listings/page.js
@@ -0,0 +1,10 @@
+import NavBar from "@/components/nav/navBar";
+
+export default function ListingsPage() {
+  return (
+    <>
+      <NavBar />
+      <div className="w-64 h-full bg-lighterGray">leftBar</div>
+    </>
+  );
+}
