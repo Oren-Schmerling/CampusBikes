5c542e714d43267c8e58ba67e405c0de27163088
jchien19 <jmchien@umass.edu>
2025-10-20 22:40:47 -0400

    Background color changes
    Changed background color in listing cards area.

--- Frontend/src/app/listings/page.js
+++ Frontend/src/app/listings/page.js
@@ -114,7 +114,7 @@ export default function ListingsPage() {
   };
 
 return (
-    <div className="flex h-screen bg-gray-50">      
+    <div className="flex h-screen bg-white">      
       {/* Filter sidebar */}
       <LeftBar />
 
